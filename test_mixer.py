e8c5df33206a82128d45974c1c2393ab477a6072
Castin <martin.castin@student.ulg.ac.be>
2017-11-25 21:29:32 +0100

    mixer for debugging

---
+++ test_mixer.py
@@ -0,0 +1,316 @@
+import os, glob
+import numpy as np
+from scipy.signal import spectrogram, stft, istft
+import random
+from pydub import AudioSegment
+
+class TestMixer:
+
+    #If you want to avoid scratching the ssd
+    #sudo mount -t tmpfs -o size=500m tmpfs tmpMixed
+    output_dir = "tmpMixed/"
+
+    #The length of the spectrogram we take
+    spec_length = 128
+    nb_freq = 128
+
+    # Difference in the speech signal levels in dB.
+    # Assumes that both audio files have been correctly normalized and have the same speech signal level initially.
+
+    def __init__(self, train=True, nbSamples = float("inf"), nbSpeakers = float("inf")):
+        self.male_audios = []
+        self.female_audios = []
+        self.indices = []
+        self.epochs_completed = 0
+        self.index_in_epoch = 0
+        self.epochs_completed_test = 0
+        self.index_in_epoch_test = 0
+
+        if train:
+            audio_dir = "Data/LibriSpeech/train-clean-100/"
+
+            female_file = "magnolia/data/librispeech/authors/train-clean-100-F.txt"
+            male_file = "magnolia/data/librispeech/authors/train-clean-100-M.txt"
+
+            self.in_data_path = "Data/train/in/spec"
+            self.out_data_path = "Data/train/out/spec"
+        else:
+            audio_dir = "Data/LibriSpeech/dev-clean/"
+
+            female_file = "magnolia/data/librispeech/authors/dev-clean-F.txt"
+            male_file = "magnolia/data/librispeech/authors/dev-clean-M.txt"
+
+            self.in_data_path = "Data/dev/in/spec"
+            self.out_data_path = "Data/dev/out/spec"
+
+        #Collect males dirs:
+        male_speaker_dirs = [];
+        with open(male_file, "r") as f:
+
+            i = 0
+            for folder in f:
+                folder = folder[:-1]
+                male_speaker_dirs[0:0] = glob.glob(os.path.join(audio_dir + folder, '*'))
+                i += 1
+                if i >= nbSpeakers:
+                    break;
+
+        #Collect females files:
+        female_speaker_dirs = [];
+        with open(female_file, "r") as f:
+
+            i= 0
+            for folder in f:
+                folder = folder[:-1]
+                female_speaker_dirs[0:0] = glob.glob(os.path.join(audio_dir + folder, '*'))
+                i += 1
+                if i >= nbSpeakers:
+                    break;
+
+        for male_dir in male_speaker_dirs:
+
+            self.male_audios[0:0] = glob.glob(os.path.join(male_dir, '*.flac'))
+
+        self.male_audios = np.random.permutation(self.male_audios)
+
+        for female_dir in female_speaker_dirs:
+            self.female_audios[0:0] = glob.glob(os.path.join(female_dir, '*.flac'))
+
+        self.female_audios = np.random.permutation(self.female_audios)
+
+        maxInd = min(nbSamples, len(self.male_audios), len(self.female_audios))
+        self.indices = range(0,maxInd)
+
+        self.sep = round(0.8*maxInd)
+        self.indices_train = self.indices[:self.sep]
+        self.indices_test = self.indices[self.sep:]
+
+        #The list function performs a shallow copy
+        self.indices_train_it = list(self.indices_train)
+        #works in place
+        random.shuffle(self.indices_train_it)
+        self.indices_train_it = iter(self.indices_train_it)
+
+        #The list function performs a shallow copy
+        self.indices_test_it = [x - self.sep for x in self.indices_test]
+        #works in place
+        random.shuffle(self.indices_test_it)
+        self.indices_test_it = iter(self.indices_test_it)
+
+
+    def next(self):
+
+        try:
+            i = next(self.indices_it)
+            self.index_in_epoch += 1
+
+        except StopIteration:
+            #The list function performs a shallow copy
+            self.indices_it = list(self.indices_train)
+
+            #works in place
+            random.shuffle(self.indices_it)
+            self.indices_it = iter(self.indices_it)
+
+            self.epochs_completed += 1
+            self.index_in_epoch = 0
+            i = next(self.indices_it)
+
+        sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
+        target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
+
+        sound2 = AudioSegment.from_file(self.female_audios[i],format='flac')
+        target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
+
+        length = min(len(target1), len(target2))
+
+        freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
+        freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
+        mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
+
+        Fxx_mixed = Pxx_target1 + Pxx_target2
+
+        Pxx_mixed = np.abs(Fxx_mixed)
+        phase_mixed = np.angle(Fxx_mixed)
+
+        return np.moveaxis(np.array([Pxx_mixed])[:, :, :self.spec_length], 0, -1), \
+               np.moveaxis(np.array([mask_target])[:, :, :self.spec_length], 0, -1), \
+               np.moveaxis(np.array([phase_mixed])[:, :, :self.spec_length], 0, -1)
+
+    def build_dataset(self):
+        indices_iterator = list(self.indices_train)
+        random.shuffle(indices_iterator)
+
+        #enumerate to take different females for the males
+        for i, j in enumerate(indices_iterator):
+
+            sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
+            target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
+
+            sound2 = AudioSegment.from_file(self.female_audios[j],format='flac')
+            target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
+
+            length = min(len(target1), len(target2))
+
+            freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
+            freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
+            mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
+
+            Fxx_mixed = Pxx_target1 + Pxx_target2
+
+            np.save(self.in_data_path + str(i), np.moveaxis(np.array([Fxx_mixed])[:, :, :self.spec_length], 0, -1))
+            np.save(self.out_data_path + str(i), np.moveaxis(np.array([mask_target])[:, :, :self.spec_length], 0, -1))
+
+    def load_dataset(self):
+        self.in_data = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
+        self.out_data = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
+
+        for i in self.indices_train:
+            self.in_data[i, :, :, :] = np.load(self.in_data_path + str(i) + ".npy")[:self.nb_freq, :, :]
+            self.out_data[i, :, :, :] = np.load(self.out_data_path + str(i) + ".npy")[:self.nb_freq, :, :]
+
+    def build_dataset_in_mem(self):
+        indices_iterator = list(self.indices_train)
+        random.shuffle(indices_iterator)
+
+        self.in_data_train = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1), dtype=complex)
+        self.out_data_train = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
+
+        #enumerate to take different females for the males
+        for i, j in enumerate(indices_iterator):
+
+            sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
+            target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
+
+            sound2 = AudioSegment.from_file(self.female_audios[j],format='flac')
+            target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
+
+            length = min(len(target1), len(target2))
+
+            freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
+            freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
+            mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
+
+            Fxx_mixed = Pxx_target1 + Pxx_target2
+
+            self.in_data_train[i, :, :, :] = np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :self.spec_length], 0, -1)
+            self.out_data_train[i, :, :, :] = np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :self.spec_length], 0, -1)
+
+        indices_iterator = list(self.indices_test)
+        random.shuffle(indices_iterator)
+
+        self.in_data_test = np.empty((len(self.indices_test), self.nb_freq, self.spec_length, 1), dtype=complex)
+        self.out_data_test = np.empty((len(self.indices_test), self.nb_freq, self.spec_length, 1))
+
+        #enumerate to take different females for the males
+        for i, j in enumerate(indices_iterator):
+
+            sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
+            target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
+
+            sound2 = AudioSegment.from_file(self.female_audios[j],format='flac')
+            target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
+
+            length = min(len(target1), len(target2))
+
+            freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
+            freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
+            mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
+
+            Fxx_mixed = Pxx_target1 + Pxx_target2
+
+            self.in_data_test[i, :, :, :] = np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :self.spec_length], 0, -1)
+            self.out_data_test[i, :, :, :] = np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :self.spec_length], 0, -1)
+
+
+    def next_load_file(self):
+        try:
+            i = next(self.indices_it)
+            self.index_in_epoch += 1
+
+        except StopIteration:
+            #The list function performs a shallow copy
+            self.indices_it = list(self.indices_train)
+
+            #works in place
+            random.shuffle(self.indices_it)
+            self.indices_it = iter(self.indices_it)
+
+            self.epochs_completed += 1
+            self.index_in_epoch = 0
+            i = next(self.indices_it)
+
+        Fxx_mixed = np.load(self.in_data_path + str(i) + ".npy")
+
+        return np.abs(Fxx_mixed), np.load(self.out_data_path + str(i) + ".npy"), np.angle(Fxx_mixed)
+
+    def next_mem_train(self):
+        try:
+            i = next(self.indices_train_it)
+            self.index_in_epoch += 1
+
+        except StopIteration:
+            #The list function performs a shallow copy
+            self.indices_train_it = list(self.indices_train)
+
+            #works in place
+            random.shuffle(self.indices_train_it)
+            self.indices_train_it = iter(self.indices_train_it)
+
+            self.epochs_completed += 1
+            self.index_in_epoch = 0
+            i = next(self.indices_train_it)
+
+        return np.abs(self.in_data_train[i]),self.out_data_train[i], np.angle(self.in_data_train[i])
+
+    def next_mem_test(self):
+        try:
+            i = next(self.indices_test_it)
+            self.index_in_epoch_test += 1
+
+        except StopIteration:
+            self.indices_test_it = [x - self.sep for x in self.indices_test]
+
+            #works in place
+            random.shuffle(self.indices_test_it)
+            self.indices_test_it = iter(self.indices_test_it)
+
+            self.epochs_completed_test += 1
+            self.index_in_epoch_test = 0
+            i = next(self.indices_test_it)
+
+        return np.abs(self.in_data_test[i]),self.out_data_test[i], np.angle(self.in_data_test[i])
+
+    def get_batch(self, size=32):
+
+        batchIn = np.empty([size, self.nb_freq, self.spec_length, 1])
+        batchOut = np.empty([size, self.nb_freq, self.spec_length, 1])
+        batchPhase = np.empty([size, self.nb_freq, self.spec_length, 1])
+
+        for i in range(0,size):
+            sample = self.next_mem_train()
+            batchIn[i, :, :, :] = sample[0][:self.nb_freq,:,:]
+            batchOut[i, :, :, :] = sample[1][:self.nb_freq,:,:]
+            batchPhase[i, :, :, :] = sample[2][:self.nb_freq, :, :]
+
+        return batchIn, batchOut, batchPhase
+
+    def get_batch_test(self, size=32):
+
+        batchIn = np.empty([size, self.nb_freq, self.spec_length, 1])
+        batchOut = np.empty([size, self.nb_freq, self.spec_length, 1])
+        batchPhase = np.empty([size, self.nb_freq, self.spec_length, 1])
+
+        for i in range(0,size):
+            sample = self.next_mem_test()
+            batchIn[i, :, :, :] = sample[0][:self.nb_freq,:,:]
+            batchOut[i, :, :, :] = sample[1][:self.nb_freq,:,:]
+            batchPhase[i, :, :, :] = sample[2][:self.nb_freq, :, :]
+
+        return batchIn, batchOut, batchPhase
+
+    def normalise_divmax(self, samples):
+
+        normalised = samples / max(samples)
+
+        return normalised
