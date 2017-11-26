8158d2f16fd2158ee221ca115a435b22ac18769b
Castin <martin.castin@student.ulg.ac.be>
2017-11-26 09:34:16 +0100

    cut small frames from files

--- test_mixer.py
+++ test_mixer.py
@@ -25,7 +25,8 @@ class TestMixer:
         self.index_in_epoch = 0
         self.epochs_completed_test = 0
         self.index_in_epoch_test = 0
-
+        self.in_file_ind_train = 0
+        self.in_file_ind_test = 0
         if train:
             audio_dir = "Data/LibriSpeech/train-clean-100/"
 
@@ -91,90 +92,23 @@ class TestMixer:
         random.shuffle(self.indices_train_it)
         self.indices_train_it = iter(self.indices_train_it)
 
+        self.current_sample_train = next(self.indices_train_it)
+
         #The list function performs a shallow copy
         self.indices_test_it = [x - self.sep for x in self.indices_test]
         #works in place
         random.shuffle(self.indices_test_it)
         self.indices_test_it = iter(self.indices_test_it)
 
+        self.current_sample_test = next(self.indices_test_it)
 
-    def next(self):
-
-        try:
-            i = next(self.indices_it)
-            self.index_in_epoch += 1
-
-        except StopIteration:
-            #The list function performs a shallow copy
-            self.indices_it = list(self.indices_train)
-
-            #works in place
-            random.shuffle(self.indices_it)
-            self.indices_it = iter(self.indices_it)
-
-            self.epochs_completed += 1
-            self.index_in_epoch = 0
-            i = next(self.indices_it)
-
-        sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
-        target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
-
-        sound2 = AudioSegment.from_file(self.female_audios[i],format='flac')
-        target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
-
-        length = min(len(target1), len(target2))
-
-        freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
-        freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
-        mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
-
-        Fxx_mixed = Pxx_target1 + Pxx_target2
-
-        Pxx_mixed = np.abs(Fxx_mixed)
-        phase_mixed = np.angle(Fxx_mixed)
-
-        return np.moveaxis(np.array([Pxx_mixed])[:, :, :self.spec_length], 0, -1), \
-               np.moveaxis(np.array([mask_target])[:, :, :self.spec_length], 0, -1), \
-               np.moveaxis(np.array([phase_mixed])[:, :, :self.spec_length], 0, -1)
-
-    def build_dataset(self):
-        indices_iterator = list(self.indices_train)
-        random.shuffle(indices_iterator)
-
-        #enumerate to take different females for the males
-        for i, j in enumerate(indices_iterator):
-
-            sound1 = AudioSegment.from_file(self.male_audios[i], format='flac')
-            target1 = self.normalise_divmax(np.array(sound1.get_array_of_samples()))
-
-            sound2 = AudioSegment.from_file(self.female_audios[j],format='flac')
-            target2 = self.normalise_divmax(np.array(sound2.get_array_of_samples()))
-
-            length = min(len(target1), len(target2))
-
-            freqs_target1, bins_target1, Pxx_target1 = stft(target1[:length])
-            freqs_target2, bins_target2, Pxx_target2 = stft(target2[:length])
-            mask_target = np.abs(Pxx_target1) / (np.abs(Pxx_target2) + np.abs(Pxx_target1) + 1e-100)
-
-            Fxx_mixed = Pxx_target1 + Pxx_target2
-
-            np.save(self.in_data_path + str(i), np.moveaxis(np.array([Fxx_mixed])[:, :, :self.spec_length], 0, -1))
-            np.save(self.out_data_path + str(i), np.moveaxis(np.array([mask_target])[:, :, :self.spec_length], 0, -1))
-
-    def load_dataset(self):
-        self.in_data = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
-        self.out_data = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
-
-        for i in self.indices_train:
-            self.in_data[i, :, :, :] = np.load(self.in_data_path + str(i) + ".npy")[:self.nb_freq, :, :]
-            self.out_data[i, :, :, :] = np.load(self.out_data_path + str(i) + ".npy")[:self.nb_freq, :, :]
 
     def build_dataset_in_mem(self):
         indices_iterator = list(self.indices_train)
         random.shuffle(indices_iterator)
 
-        self.in_data_train = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1), dtype=complex)
-        self.out_data_train = np.empty((len(self.indices_train), self.nb_freq, self.spec_length, 1))
+        self.in_data_train = []
+        self.out_data_train = []
 
         #enumerate to take different females for the males
         for i, j in enumerate(indices_iterator):
@@ -193,14 +127,14 @@ class TestMixer:
 
             Fxx_mixed = Pxx_target1 + Pxx_target2
 
-            self.in_data_train[i, :, :, :] = np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :self.spec_length], 0, -1)
-            self.out_data_train[i, :, :, :] = np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :self.spec_length], 0, -1)
+            self.in_data_train.append(np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :], 0, -1))
+            self.out_data_train.append(np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :], 0, -1))
 
         indices_iterator = list(self.indices_test)
         random.shuffle(indices_iterator)
 
-        self.in_data_test = np.empty((len(self.indices_test), self.nb_freq, self.spec_length, 1), dtype=complex)
-        self.out_data_test = np.empty((len(self.indices_test), self.nb_freq, self.spec_length, 1))
+        self.in_data_test = []
+        self.out_data_test = []
 
         #enumerate to take different females for the males
         for i, j in enumerate(indices_iterator):
@@ -219,35 +153,18 @@ class TestMixer:
 
             Fxx_mixed = Pxx_target1 + Pxx_target2
 
-            self.in_data_test[i, :, :, :] = np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :self.spec_length], 0, -1)
-            self.out_data_test[i, :, :, :] = np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :self.spec_length], 0, -1)
+            self.in_data_test.append(np.moveaxis(np.array([Fxx_mixed])[:, :self.nb_freq, :], 0, -1))
+            self.out_data_test.append(np.moveaxis(np.array([mask_target])[:, :self.nb_freq, :], 0, -1))
 
-
-    def next_load_file(self):
-        try:
-            i = next(self.indices_it)
-            self.index_in_epoch += 1
-
-        except StopIteration:
-            #The list function performs a shallow copy
-            self.indices_it = list(self.indices_train)
-
-            #works in place
-            random.shuffle(self.indices_it)
-            self.indices_it = iter(self.indices_it)
-
-            self.epochs_completed += 1
-            self.index_in_epoch = 0
-            i = next(self.indices_it)
-
-        Fxx_mixed = np.load(self.in_data_path + str(i) + ".npy")
-
-        return np.abs(Fxx_mixed), np.load(self.out_data_path + str(i) + ".npy"), np.angle(Fxx_mixed)
 
     def next_mem_train(self):
         try:
-            i = next(self.indices_train_it)
             self.index_in_epoch += 1
+            self.in_file_ind_train += self.spec_length
+            if self.in_file_ind_train > self.in_data_train[self.current_sample_train].shape[1]:
+
+                self.current_sample_train = next(self.indices_train_it)
+                self.in_file_ind_train = self.spec_length
 
         except StopIteration:
             #The list function performs a shallow copy
@@ -261,12 +178,18 @@ class TestMixer:
             self.index_in_epoch = 0
             i = next(self.indices_train_it)
 
-        return np.abs(self.in_data_train[i]),self.out_data_train[i], np.angle(self.in_data_train[i])
+        return np.abs(self.in_data_train[i][:,self.in_file_ind_train-self.spec_length:self.in_file_ind_train, :]),
+                self.out_data_train[i][:,self.in_file_ind_train-self.spec_length:self.in_file_ind_train, :],
+                np.angle(self.in_data_train[i][:,self.in_file_ind_train-self.spec_length:self.in_file_ind_train, :])
 
     def next_mem_test(self):
         try:
-            i = next(self.indices_test_it)
-            self.index_in_epoch_test += 1
+            self.index_in_epoch += 1
+            self.in_file_ind_test += self.spec_length
+            if self.in_file_ind_test > self.in_data_test[self.current_sample_test].shape[1]:
+
+                self.current_sample_test = next(self.indices_test_it)
+                self.in_file_ind_test = self.spec_length
 
         except StopIteration:
             self.indices_test_it = [x - self.sep for x in self.indices_test]
@@ -279,7 +202,9 @@ class TestMixer:
             self.index_in_epoch_test = 0
             i = next(self.indices_test_it)
 
-        return np.abs(self.in_data_test[i]),self.out_data_test[i], np.angle(self.in_data_test[i])
+        return np.abs(self.in_data_test[i][:,self.in_file_ind_test-self.spec_length:self.in_file_ind_test, :]),
+                self.out_data_test[i][:,self.in_file_ind_test-self.spec_length:self.in_file_ind_test, :],
+                np.angle(self.in_data_test[i][:,self.in_file_ind_test-self.spec_length:self.in_file_ind_test, :])
 
     def get_batch(self, size=32):
 
